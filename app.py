30dfdb729e8921812af16048a2cc230a25d1038c
Maharaj Mahaadev <maharajmahaadev2@gmail.com>
2024-11-15 20:05:51 +0530

    Update app.py imports error

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, make_reponse
+from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 from deepface import DeepFace
 import base64
